ba0628219479533a9863f4d2c8508fdebe708c99
SKIPAH <juzzii.mehta@gmail.com>
2022-10-20 19:56:42 +0300

    SPA first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import ContactUs from './components/ContactUs';
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Header/>
+      <div classname='container'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="*"element={<NotFound />}/>
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
